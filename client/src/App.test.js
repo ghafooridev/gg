bef9c7c3bec6dcae177c3381bd7dbe38a7c7b4ae
AhanM <ahan.m13@gmail.com>
2020-08-15 22:05:35 -0700

    simple jest testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders create room', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/create room/i);
   expect(linkElement).toBeInTheDocument();
 });
