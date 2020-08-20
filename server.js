288fc1c4b8b669c43e6d1c2a8bc134721bf1511d
AhanM <ahan.m13@gmail.com>
2020-08-19 23:18:38 -0700

    added test server endpoint

--- server.js
+++ server.js
@@ -133,5 +133,9 @@ if (process.env.PROD) {
     });
 }
 
+app.get('/test', (req, res) => {
+  res.send('test passed.')
+})
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`server is running on port ${port}`));
