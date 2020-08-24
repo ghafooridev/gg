2088ab6d104f18d03b6d7b202de3338f8361cb22
AhanM <ahan.m13@gmail.com>
2020-08-23 21:18:40 -0700

    Added login modal in landing page

--- client/src/views/pages/LandingPage.js
+++ client/src/views/pages/LandingPage.js
@@ -1,27 +1,12 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, { useState } from "react";
 
 // reactstrap components
 import {
   Button,
   Form,
+  FormGroup,
+  FormText,
+  Label,
   Input,
   InputGroupAddon,
   InputGroupText,
@@ -36,117 +21,129 @@ import LandingNavbar from "../../components/Navbars/LandingNavbar.js";
 import LandingPageHeader from "../../components/Headers/LandingPageHeader.js";
 import LandingFooter from "../../components/Footers/LandingFooter.js";
 import GameCard from "components/GameCard.js";
+import LoginModal from "./LoginModal.js";
 
-function LandingPage() {
+const LandingPage = () => {
+  const [isLoggedin, setLoggedin] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const [university, setUniversity] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
   document.documentElement.classList.remove("nav-open");
+  
   React.useEffect(() => {
     document.body.classList.add("profile-page");
     return function cleanup() {
       document.body.classList.remove("profile-page");
     };
   });
+
+  function handleSignin() {
+    modalOpen();
+  }
+
+  // TODO: handle sign up button press event
+  function handleSignup() {
+
+  }
+
+  function modalOpen() {
+    setShowModal(true);
+  }
+
+  function modalClose() {
+    setLoggedin(true);
+    setShowModal(false);
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+
+    if (name === "name") {
+      setName(value);
+    } else if(name === "email") {
+      setEmail(value);
+    } else if(name === "university") {
+      setUniversity(value);
+    } else if(name === "description") {
+      setDescription(value);
+    }
+  }
+
   return (
     <>
       <LandingNavbar />
-      <LandingPageHeader />
+      <LandingPageHeader handleSignin={handleSignin} handleSignup={handleSignup} 
+        isLoggedin={isLoggedin} username={name} />
+
+      <LoginModal show={showModal}>
+        <form onSubmit={e => e.preventDefault()}>
+          <FormGroup>
+            <Label for="Name">First Name</Label>
+            <Input
+              type="text"
+              name="name"
+              id="Name"
+              placeholder="Enter First Name"
+              onChange={e => handleChange(e)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="emailAdd">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="emailAdd"
+              placeholder="Enter Email"
+              onChange={e => handleChange(e)}
+            />
+            <FormText color="muted">
+              We'll never share your information with anyone else.
+            </FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="University">University Name</Label>
+            <Input
+              type="text"
+              name="university"
+              id="University"
+              placeholder="Arizona State University"
+              autoComplete="off"
+              onChange={e => handleChange(e)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="Description">Description</Label>
+            <Input
+              type="text"
+              name="description"
+              id="Description"
+              placeholder="I love playing chess and surfing"
+              autoComplete="off"
+              onChange={e => handleChange(e)}
+            />
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />{' '}
+              Check me out
+              <span className="form-check-sign">
+                <span className="check"></span>
+            </span>
+            </Label>
+          </FormGroup>
+          <Button color="primary" type="submit" onClick={e => modalClose(e)}>
+            Submit
+          </Button>
+        </form>
+      </LoginModal>
+
       <div className="main">
-        {/* <div className="section text-center"> */}
-          {/* <Container>
-            <Row>
-              <Col className="ml-auto mr-auto" md="8">
-                <h2 name="Games" className="title">Let's talk product</h2>
-                <h5 className="description">
-                  This is the paragraph where you can write more details about
-                  your product. Keep you user engaged by providing meaningful
-                  information. Remember that by this time, the user is curious,
-                  otherwise he wouldn't scroll to get here. Add a button if you
-                  want the user to see more.
-                </h5>
-                <br />
-                <Button
-                  className="btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  See Details
-                </Button>
-              </Col>
-            </Row>
-            <br />
-            <br />
-            <Row>
-              <Col md="3">
-                <div className="info">
-                  <div className="icon icon-info">
-                    <i className="nc-icon nc-album-2" />
-                  </div>
-                  <div className="description">
-                    <h4 className="info-title">Beautiful Gallery</h4>
-                    <p className="description">
-                      Spend your time generating new ideas. You don't have to
-                      think of implementing.
-                    </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
-                  </div>
-                </div>
-              </Col>
-              <Col md="3">
-                <div className="info">
-                  <div className="icon icon-info">
-                    <i className="nc-icon nc-bulb-63" />
-                  </div>
-                  <div className="description">
-                    <h4 className="info-title">New Ideas</h4>
-                    <p>
-                      Larger, yet dramatically thinner. More powerful, but
-                      remarkably power efficient.
-                    </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
-                  </div>
-                </div>
-              </Col>
-              <Col md="3">
-                <div className="info">
-                  <div className="icon icon-info">
-                    <i className="nc-icon nc-chart-bar-32" />
-                  </div>
-                  <div className="description">
-                    <h4 className="info-title">Statistics</h4>
-                    <p>
-                      Choose from a veriety of many colors resembling sugar
-                      paper pastels.
-                    </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
-                  </div>
-                </div>
-              </Col>
-              <Col md="3">
-                <div className="info">
-                  <div className="icon icon-info">
-                    <i className="nc-icon nc-sun-fog-29" />
-                  </div>
-                  <div className="description">
-                    <h4 className="info-title">Delightful design</h4>
-                    <p>
-                      Find unique and handmade delightful designs related items
-                      directly from our sellers.
-                    </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </Container> */}
-        {/* </div> */}
         <div className="section section-dark text-center" id="games">
           <Container>
             <h2 name="About" className="title">Games</h2>
@@ -215,6 +212,7 @@ function LandingPage() {
           </Container>
         </div>
       </div>
+      
       <LandingFooter />
     </>
   );
