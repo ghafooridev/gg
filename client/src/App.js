088f2047f09892a62331554ffa29c2ec6ce6c5d5
Kush Parmar <igneel.kush73@gmail.com>
2020-08-29 16:01:24 -0700

    adds custom modular scss files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 // styles
-import "./assets/css/bootstrap.min.css";
-import "./assets/scss/paper-kit.scss";
-import "./assets/demo/demo.css";
+import './assets/css/bootstrap.min.css';
+import './assets/scss/paper-kit.scss';
+import './assets/demo/demo.css';
+import './assets/scss/custom/main.scss';
 
 // import CreateRoom from './routes/createRoom';
 import Room from './routes/room';
@@ -14,16 +15,16 @@ import LandingPage from './views/pages/LandingPage';
 import Lobby from './views/pages/Lobby';
 
 function App() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/login" exact component={LandingPage} />
-          <Route path="/room/:roomID" component={Room} />
-          <Route path="/lobby" component={Lobby} />
-        </Switch>
-      </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={LandingPage} />
+				<Route path="/login" exact component={LandingPage} />
+				<Route path="/room/:roomID" component={Room} />
+				<Route path="/lobby" component={Lobby} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
