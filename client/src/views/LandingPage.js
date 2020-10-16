06cf604a476f40902f76d6bb25a06c7b70d3774a
Ali Ghafoori <ghafooridev@gmail.com>
2020-10-16 11:49:31 +0330

    test commit on bitbucket

--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -24,6 +24,7 @@ import LandingPageHeader from '../components/Headers/LandingPageHeader.js';
 import LandingFooter from '../components/Footers/LandingFooter.js';
 import GameCard from 'components/GameCard.js';
 import authenticationService from 'services/authentication.service.js';
+import Feedback from './Feedback';
 
 const LandingPage = (props) => {
   const loggedInStartState = authenticationService.currentUserValue
@@ -398,54 +399,7 @@ const LandingPage = (props) => {
             </div>
           )}
         </div>
-        <div className="section landing-section">
-          <Container>
-            <Row>
-              <Col className="ml-auto mr-auto" md="8">
-                <h2 className="text-center">Feedback?</h2>
-                <Form className="contact-form">
-                  <Row>
-                    <Col md="6">
-                      <label>Name</label>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="nc-icon nc-single-02" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="Name" type="text" />
-                      </InputGroup>
-                    </Col>
-                    <Col md="6">
-                      <label>Email</label>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="nc-icon nc-email-85" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="Email" type="text" />
-                      </InputGroup>
-                    </Col>
-                  </Row>
-                  <label>Message</label>
-                  <Input
-                    placeholder="Tell us your thoughts and feelings..."
-                    type="textarea"
-                    rows="4"
-                  />
-                  <Row>
-                    <Col className="ml-auto mr-auto" md="4">
-                      <Button className="btn-fill" color="danger" size="lg">
-                        Send Message
-                      </Button>
-                    </Col>
-                  </Row>
-                </Form>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+       <Feedback/>
       </div>
 
       <LandingFooter />
