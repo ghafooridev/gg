319f380f9181de810a47ebf2709fef6790531e89
Ali Ghafoori <ghafooridev@gmail.com>
2020-10-18 15:31:39 +0330

    add drop down

--- client/src/components/sharedComponents/DropDown.jsx
+++ client/src/components/sharedComponents/DropDown.jsx
@@ -1,28 +1,81 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import PropTypes from 'prop-types';
 
-import {Input, InputGroup, InputGroupAddon, InputGroupText} from "reactstrap";
+import {Input, InputGroup, InputGroupAddon, InputGroupText, ListGroup, ListGroupItem} from "reactstrap";
 
 const DropDown = function (props) {
+	const {label, placeholder, name, option, onSelect} = props;
+	const [list, setList] = useState(option);
+	const [selectedData, setSelectedData] = useState('');
+	const [show, setShow] = useState(false)
 
-	const {label, placeholder, addonType} = props;
+	const onChangeText = function (event) {
+		const {value} = event.target;
+
+		if (value) {
+			const filteredList = option.filter(item => item.value.startsWith(value.toLowerCase()));
+			setShow(true);
+			setSelectedData(value)
+			return setList(filteredList);
+		}
+
+		return setList(option);
+	}
+
+	const onDropDownSelect = function (item) {
+		setSelectedData(item);
+		setShow(false);
+	};
+
+	const onPressEnter = function (event) {
+		if (event.which === 13 && list.length) {
+			setSelectedData(list[0]);
+			setShow(false);
+		}
+	}
+
+	const onClickIcon = function () {
+		setShow(!show);
+	}
+
+	useEffect(() => {
+		if (selectedData.id) {
+			onSelect({name, value: selectedData.id});
+		}
+	}, [selectedData])
 
 	return (
 		<>
 			<label>{label}</label>
 			<InputGroup>
-				<InputGroupAddon addonType={addonType}>
+				<InputGroupAddon addonType='prepend'>
 					<InputGroupText>
-						<i className="nc-icon nc-minimal-down"/>
+						<i className={show ? 'nc-icon nc-minimal-up' : 'nc-icon nc-minimal-down'} onClick={onClickIcon}/>
 					</InputGroupText>
-				</InputGroupAddon>}
+				</InputGroupAddon>
 				<Input
 					placeholder={placeholder}
 					type='text'
-					{...props}
+					value={selectedData.value}
+					onChange={onChangeText}
+					onKeyPress={onPressEnter}
 				/>
 			</InputGroup>
+			<ListGroup>
+				{show && list.map((item, index) => {
+					return (
+						<ListGroupItem
+							style={{cursor: 'pointer'}}
+							key={index}
+							onClick={() => onDropDownSelect(item)}
+						>{
+							item.value}
+						</ListGroupItem>
+					)
+				})
+				}
+			</ListGroup>
 		</>
 	)
 }
@@ -30,16 +83,8 @@ const DropDown = function (props) {
 DropDown.propTypes = {
 	label: PropTypes.string,
 	placeholder: PropTypes.string,
-	icon: PropTypes.element,
-	addonType: PropTypes.string,
-	rows:PropTypes.string,
-	type:PropTypes.string
-};
-
-DropDown.defaultProps = {
-	addonType: 'prepend',
-	rows:false,
-	type:'text'
+	option: PropTypes.array,
+	onSelect: PropTypes.func
 };
 
 export default DropDown
---
+++ client/src/utils/Constant.js
@@ -0,0 +1,10 @@
+const Constant = {
+	ENUMS: {
+		FEEDBACK_CATEGORY: [
+			{id: 'CAT1', value: 'cat1'},
+			{id: 'CAT2', value: 'cat2'},
+		]
+	}
+
+};
+export default Constant;
