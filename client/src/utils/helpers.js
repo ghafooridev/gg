96650dffe52e46760c76f52d87fd316c64d620be
ali ghafoori <ghafooridev@gmail.com>
2020-12-25 20:45:43 +0330

    change env variable

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,22 +1,23 @@
-import Constant from "../utils/Constant"
-import Storage from "../services/Storage"
+import Constant from "../utils/Constant";
+import Storage from "../services/Storage";
 
 export const socketURL = function () {
+  console.log(process.env);
   if (process.env.DEBUG) {
-    return "localhost:5000"
+    return "localhost:5000";
   }
 
-  return "www.ggchat.io"
-}
+  return "www.ggchat.io";
+};
 
-export const getUserNameFromLocalStorage=function (history){
-  const currentLink=window.location.pathname
-  const currentUser = Storage.pull(Constant.STORAGE.CURRENT_USER)
-  if(currentUser){
-    return currentUser.username
+export const getUserNameFromLocalStorage = function (history) {
+  const currentLink = window.location.pathname;
+  const currentUser = Storage.pull(Constant.STORAGE.CURRENT_USER);
+  if (currentUser) {
+    return currentUser.username;
   }
 
-  Storage.push(Constant.STORAGE.CURRENT_LINK, JSON.stringify(currentLink))
+  Storage.push(Constant.STORAGE.CURRENT_LINK, JSON.stringify(currentLink));
 
-  return history.push("/login")
-}
+  return history.push("/login");
+};
